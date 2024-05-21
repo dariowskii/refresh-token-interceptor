68773b795683d43576bccebab0254860710ba65b
Dario Varriale <dario.varriale@atobit.it>
2024-05-21 22:19:48 +0200

    Implemented LocalData class

---
+++ lib/utils/local_data.dart
@@ -0,0 +1,38 @@
+import 'package:refresh_token_interceptor/models.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const kAuthTokenKey = 'authToken';
+const kRefreshTokenKey = 'refreshToken';
+
+class LocalData {
+  LocalData._();
+
+  static final LocalData _instance = LocalData._();
+  static LocalData get instance => _instance;
+
+  Future<String?> get authToken async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(kAuthTokenKey);
+  }
+
+  Future<String?> get refreshToken async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(kRefreshTokenKey);
+  }
+
+  Future<void> saveToken(UserAuthResponse response) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await Future.wait([
+      prefs.setString(kAuthTokenKey, response.token),
+      prefs.setString(kRefreshTokenKey, response.refreshToken)
+    ]);
+  }
+
+  Future<void> clearToken() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await Future.wait(
+        [prefs.remove(kAuthTokenKey), prefs.remove(kRefreshTokenKey)]);
+  }
+}
