49bd2658096e222090eed54c3e2945191467d822
Dario Varriale <dario.varriale@atobit.it>
2024-06-01 14:48:58 +0200

    Updated path name

--- lib/riverpod/auth_repository_provider/auth_repository_provider.dart
+++ lib/riverpod/auth_repository_provider/auth_repository_provider.dart
@@ -2,7 +2,7 @@ import 'package:refresh_token_interceptor/auth_repository/auth_repository.dart';
 import 'package:refresh_token_interceptor/riverpod/http_client/http_client.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'auth_repository.g.dart';
+part 'auth_repository_provider.g.dart';
 
 @riverpod
 AuthRepository authRepository(AuthRepositoryRef ref) {
--- lib/riverpod/auth_repository_provider/auth_repository_provider.g.dart
+++ lib/riverpod/auth_repository_provider/auth_repository_provider.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'auth_repository.dart';
+part of 'auth_repository_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
